73ea414fc8f02e025f7ebe194c36b47d762b8a5c
Yavuz SENGOZ <github@yavuzsengoz.com>
2022-12-24 13:12:24 +0300

    Section 8: Pipelining Commands / Running Multiple Commands at the Same Time

--- rbay/sandbox/index.ts
+++ rbay/sandbox/index.ts
@@ -2,18 +2,25 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	await client.hSet('car', {
+	await client.hSet('car1', {
 		color: 'red',
 		year: 1950
 	});
+	await client.hSet('car2', {
+		color: 'green',
+		year: 1955
+	});
+	await client.hSet('car3', {
+		color: 'blue',
+		year: 1960
+	});
 
-	const car = await client.hGetAll('car');
+	const commands = [1, 2, 3].map((id) => {
+		return client.hGetAll('car' + id);
+	});
 
-	if (Object.keys(car).length === 0) {
-		console.log('Car not found, respond with 404');
-		return;
-	}
+	const results = await Promise.all(commands);
 
-	console.log(car);
+	console.log(results);
 };
 run();
