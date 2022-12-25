c8b45b563dcbfbaf3d53c37fa8da8766a7fbb465
Yavuz SENGOZ <github@yavuzsengoz.com>
2022-12-25 17:35:50 +0300

    Section 13: From Relational Data to Redis / Parsing SORT Output

--- rbay/src/services/queries/items/by-views.ts
+++ rbay/src/services/queries/items/by-views.ts
@@ -1,9 +1,17 @@
 import { client } from '$services/redis';
 import { itemsKey, itemsByViewsKey } from '$services/keys';
+import { deserialize } from './deserialize';
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
-	const results = await client.sort(itemsByViewsKey(), {
-		GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
+	let results: any = await client.sort(itemsByViewsKey(), {
+		GET: [
+			'#',
+			`${itemsKey('*')}->name`,
+			`${itemsKey('*')}->views`,
+			`${itemsKey('*')}->endingAt`,
+			`${itemsKey('*')}->imageUrl`,
+			`${itemsKey('*')}->price`
+		],
 		BY: 'nosort',
 		DIRECTION: order,
 		LIMIT: {
@@ -12,5 +20,13 @@ export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, c
 		}
 	});
 
-	console.log(results);
+	const items = [];
+	while (results.length) {
+		const [id, name, views, endingAt, imageUrl, price, ...rest] = results;
+		const item = deserialize(id, { name, views, endingAt, imageUrl, price });
+		items.push(item);
+		results = rest;
+	}
+
+	return items;
 };
