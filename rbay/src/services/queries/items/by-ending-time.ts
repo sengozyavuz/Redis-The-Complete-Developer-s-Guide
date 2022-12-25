92791def151ce47dd6f543fd22d4cb29b21355b1
Yavuz SENGOZ <github@yavuzsengoz.com>
2022-12-25 17:22:53 +0300

    Section 13: From Relational Data to Redis / Relational Data with SORT

--- rbay/src/services/queries/items/by-ending-time.ts
+++ rbay/src/services/queries/items/by-ending-time.ts
@@ -13,6 +13,5 @@ export const itemsByEndingTime = async (order: 'DESC' | 'ASC' = 'DESC', offset =
 
 	const results = await Promise.all(ids.map((id) => client.hGetAll(itemsKey(id))));
 
-	console.log(results);
 	return results.map((item, i) => deserialize(ids[i], item));
 };
