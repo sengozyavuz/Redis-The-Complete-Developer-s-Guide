f9bc816026a93a551238bb88db952d545103a97d
Yavuz SENGOZ <github@yavuzsengoz.com>
2022-12-26 02:58:18 +0300

    Bölüm 16: More Practice with the E-Commerce App / More on Items by Price

--- rbay/src/services/queries/bids.ts
+++ rbay/src/services/queries/bids.ts
@@ -1,5 +1,5 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
-import { bidHistoryKey, itemsKey } from '$services/keys';
+import { bidHistoryKey, itemsKey, itemsByPriceKey } from '$services/keys';
 import { client } from '$services/redis';
 import { DateTime } from 'luxon';
 import { getItem } from './items';
@@ -29,6 +29,10 @@ export const createBid = async (attrs: CreateBidAttrs) => {
 				bids: item.bids + 1,
 				price: attrs.amount,
 				highestBidUserId: attrs.userId
+			})
+			.zAdd(itemsByPriceKey(), {
+				value: item.id,
+				score: attrs.amount
 			})
 			.exec();
 	});
