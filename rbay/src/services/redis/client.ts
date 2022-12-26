7d8ac1abaadb60520929331b3c0d202db8c1ab14
Yavuz SENGOZ <github@yavuzsengoz.com>
2022-12-26 05:43:15 +0300

    SEction17: Extending Redis with Scripting / Code Cleanup

--- rbay/src/services/redis/client.ts
+++ rbay/src/services/redis/client.ts
@@ -44,12 +44,6 @@ const client = createClient({
 	}
 });
 
-client.on('connect', async () => {
-	await client.addOneAndStore('books:count', 10);
-	const result = await client.get('books:count');
-	console.log(result);
-});
-
 client.on('error', (err) => console.error(err));
 client.connect();
 
