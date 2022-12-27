650e894f7e4f002cef7cd3365bb96f0464579fd9
Yavuz SENGOZ <github@yavuzsengoz.com>
2022-12-27 21:43:49 +0300

    Section 20: Search in Action / Deserializing Search Results

--- rbay/src/services/queries/items/by-user.ts
+++ rbay/src/services/queries/items/by-user.ts
@@ -27,10 +27,10 @@ export const itemsByUser = async (userId: string, opts: QueryOpts) => {
 		}
 	} as any);
 
-	console.log(total, documents);
-
 	return {
-		totalPages: 0,
-		items: []
+		totalPages: Math.ceil(total / opts.perPage),
+		items: documents.map(({ id, value }) => {
+			return deserialize(id.replace('items#', ''), value as any);
+		})
 	};
 };
