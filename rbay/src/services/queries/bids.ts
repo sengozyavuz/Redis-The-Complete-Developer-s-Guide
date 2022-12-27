dc5d6d842595c8cf35b05a6b930033b4ea4f589b
Yavuz SENGOZ <github@yavuzsengoz.com>
2022-12-27 04:11:32 +0300

    Section 18: Understanding and Solving Concurrency Issues / It Works!

--- rbay/src/services/queries/bids.ts
+++ rbay/src/services/queries/bids.ts
@@ -14,9 +14,9 @@ export const createBid = async (attrs: CreateBidAttrs) => {
 		if (!item) {
 			throw new Error('Item does not exist');
 		}
-		if (item.price >= attrs.amount) {
-			throw new Error('Bid too low');
-		}
+		// if (item.price >= attrs.amount) {
+		// 	throw new Error('Bid too low');
+		// }
 		if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
 			throw new Error('Item closed to bidding');
 		}
@@ -37,37 +37,37 @@ export const createBid = async (attrs: CreateBidAttrs) => {
 		]);
 	});
 
-	return client.executeIsolated(async (isolatedClient) => {
-		await isolatedClient.watch(itemsKey(attrs.itemId));
+	// return client.executeIsolated(async (isolatedClient) => {
+	// 	await isolatedClient.watch(itemsKey(attrs.itemId));
 
-		const item = await getItem(attrs.itemId);
+	// 	const item = await getItem(attrs.itemId);
 
-		if (!item) {
-			throw new Error('Item does not exist');
-		}
-		if (item.price >= attrs.amount) {
-			throw new Error('Bid too low');
-		}
-		if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
-			throw new Error('Item closed to bidding');
-		}
+	// 	if (!item) {
+	// 		throw new Error('Item does not exist');
+	// 	}
+	// 	if (item.price >= attrs.amount) {
+	// 		throw new Error('Bid too low');
+	// 	}
+	// 	if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+	// 		throw new Error('Item closed to bidding');
+	// 	}
 
-		const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+	// 	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
 
-		return isolatedClient
-			.multi()
-			.rPush(bidHistoryKey(attrs.itemId), serialized)
-			.hSet(itemsKey(item.id), {
-				bids: item.bids + 1,
-				price: attrs.amount,
-				highestBidUserId: attrs.userId
-			})
-			.zAdd(itemsByPriceKey(), {
-				value: item.id,
-				score: attrs.amount
-			})
-			.exec();
-	});
+	// 	return isolatedClient
+	// 		.multi()
+	// 		.rPush(bidHistoryKey(attrs.itemId), serialized)
+	// 		.hSet(itemsKey(item.id), {
+	// 			bids: item.bids + 1,
+	// 			price: attrs.amount,
+	// 			highestBidUserId: attrs.userId
+	// 		})
+	// 		.zAdd(itemsByPriceKey(), {
+	// 			value: item.id,
+	// 			score: attrs.amount
+	// 		})
+	// 		.exec();
+	// });
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
